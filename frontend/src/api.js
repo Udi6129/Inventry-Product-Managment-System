063099d9c354da73f1042306f6722332da0c3a94
Udit Bhardwaj <bhardwajudit9715@gmail.com>
2025-11-21 00:08:18 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventry-product-managment-system-backend.onrender.com", // <- Backend deploy URL daalna
+  baseURL: "https://inventry-product-managment-system-backend.onrender.com/api", // <- Backend deploy URL daalna
 });
 
 export default api;
